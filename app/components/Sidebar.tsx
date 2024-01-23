a661c810491d1df3f355619dbffad03c2c285bf5
Swarnendu <swarnendumaity633@gmail.com>
2024-01-23 12:50:03 +0530

    Project

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaXTwitter } from "react-icons/fa6";
+
+function Sidebar() {
+  return (
+    <div className='col-span-3 '>
+      <FaXTwitter />
+    </div>
+  )
+}
+
+export default Sidebar
