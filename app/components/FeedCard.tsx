f29d1326be92cd947ee7bfa0ee90083c79800a22
Swarnendu <swarnendumaity633@gmail.com>
2024-01-27 01:28:46 +0530

    Project

---
+++ app/components/FeedCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+
+const FeedCard: React.FC =()=> {
+  return (
+    <div className='border'>
+      <div className='grid grid-cols-12 '>
+       <div className='col-span-4'>
+        <Image 
+        src="/myImage1.png"
+        alt="user"
+        width={50}
+        height={50}
+         />
+       </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeedCard
