f72e2aabaeb1b4911cd050f58aa3b1e5a19b5989
Swarnendu <swarnendumaity633@gmail.com>
2024-07-03 16:03:38 +0530

    Updates

--- graphql/query/user.ts
+++ graphql/query/user.ts
@@ -5,3 +5,15 @@ query VerifyUserGoogleToken($token:String!){
 verifyGoogleToken(token: $token)
 }
 `)
+
+export const getCurrentUserQuery = graphql(`
+ query GetCurrentUser {
+    getCurrentUser{
+        id
+        profileImageURL
+        email
+        firstName
+        lastName
+    }
+ }
+`)
